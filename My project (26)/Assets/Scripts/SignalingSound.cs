da0be81c2f68abc386f5863ea8c9a2038be8fdb1
Porton12345 <vkruk123@mail.ru>
2024-03-06 16:31:27 +0300

    2

--- My project (26)/Assets/Scripts/SignalingSound.cs
+++ My project (26)/Assets/Scripts/SignalingSound.cs
@@ -8,20 +8,40 @@ public class SignalingSound : MonoBehaviour
     [SerializeField, Min(0)] private float _countSecondsBeforeSignal;
 
     private Single _currentVolume = 0f;
+    private float _minVolume = 0f;
     private float _maxVolume = 1f;    
-    private float _recoveryRate = 0.2f;
+    private float _recoveryRate = 0.2f;    
 
     public void SetSignaling(AudioClip clip)
     {
+        StopAllCoroutines();
         var wait = new WaitForSeconds(_countSecondsBeforeSignal);
-        StartCoroutine(Signaling(wait, clip));        
+        StartCoroutine(StartSignaling(wait, clip));        
     }
 
-    private IEnumerator Signaling(WaitForSeconds wait, AudioClip clip)
+    public void StopSignaling(AudioClip clip)
+    {
+        StopAllCoroutines();
+        var wait = new WaitForSeconds(_countSecondsBeforeSignal);
+        StartCoroutine(StopSignaling(wait, clip));
+    }
+
+    private IEnumerator StartSignaling(WaitForSeconds wait, AudioClip clip)
     {
         while (true)
         {
             _currentVolume = Mathf.MoveTowards(_currentVolume, _maxVolume, _recoveryRate);
+            _audioSource.PlayOneShot(clip, _currentVolume);
+            Debug.Log(_currentVolume);
+            yield return wait;
+        }
+    }
+
+    private IEnumerator StopSignaling(WaitForSeconds wait, AudioClip clip)
+    {       
+        while (true)
+        {
+            _currentVolume = Mathf.MoveTowards(_currentVolume, _minVolume, _recoveryRate);
             _audioSource.PlayOneShot(clip, _currentVolume);
             Debug.Log(_currentVolume);
             yield return wait;
--- My project (26)/Assets/Scripts/SignalingSoundZone.cs
+++ My project (26)/Assets/Scripts/SignalingSoundZone.cs
@@ -1,24 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class SignalingSoundZone : MonoBehaviour
 {
-    [SerializeField] private AudioClip _clip;
-    [SerializeField] private AudioClip _noneClip;
+    [SerializeField] private AudioClip _clip;    
     [SerializeField] private SignalingSound _signalingSound;     
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<SignalingListener>() == false)
-            return;              
-
+            return;    
+        
         _signalingSound.SetSignaling(_clip);           
     }
 
     private void OnTriggerExit2D(Collider2D other)
-    {      
-        _signalingSound.SetSignaling(_noneClip);        
+    {              
+        _signalingSound.StopSignaling(_clip);        
     }
 }
